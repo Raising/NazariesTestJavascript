684bf61b8a2dcd99ecbd1eb9a2e9419161bad1c6
Ignacio Medina Castillo <raising_spirit@hotmail.com>
2017-09-30 09:59:08 +0200

    Change file names

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-ROOT = __dirname+"\\";
+'use strict';
+let ROOT = __dirname+"\\";
 
 const WebResource =require(ROOT+'classes\\webResource.js');
 const TextParser =require(ROOT+'classes\\textParser.js');
-let plansAndPricingConfiguration = require(ROOT+'configuration\\textParserConfigurtations.js').plansAndPricingConfiguration;
+let plansAndPricingConfiguration = require(ROOT+'configuration\\textParserConfigurtations.js').plansAndPricing;
 
 let monitorWebResource = new WebResource({url:'https://www.port-monitor.com/plans-and-pricing'});
-let plansAndPricingParser = new TextParser({configuration:plansAndPricingConfiguration});
+let plansAndPricingParser = new TextParser({configuration: plansAndPricingConfiguration});
+
+
 
 monitorWebResource.fetch()
 .then((resource)=>{
-	let monitorWebPlans = plansAndPricingParser.parse({mainString:resource.toString()});
-	console.log(monitorWebPlans.products);
-});
+	return plansAndPricingParser.parse({mainString:resource.getBody()});
+})
+.then((monitorWebPlans) => {
+	printPrettyJSON(monitorWebPlans);
+}) 
+.catch(console.error.bind(console));
+
+
+function printPrettyJSON(object){
+	console.log(JSON.stringify(object, null, 2));
+}
